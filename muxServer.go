1891cb0b182c7eacef921e650edcc5ab0e4e2f7c
fabio <invillia.fabio.apolonio@ext.gympass.com>
2021-10-19 11:07:38 -0300

    add update method

--- muxServer.go
+++ muxServer.go
@@ -23,16 +23,17 @@ func handleRequestsMux() {
 	myRouter.HandleFunc("/articles", returnAllArticles)
 	myRouter.HandleFunc("/article/{id}", returnSingleArticle).Methods("GET")
 	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
 	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	key := vars["id"]
+	id := vars["id"]
 
 	for _, article := range Articles {
-		if article.Id == key {
+		if article.Id == id {
 			json.NewEncoder(w).Encode(article)
 		}
 	}
@@ -44,6 +45,22 @@ func createNewArticle(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(reqBody, &article)
 
 	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+
+	for i, _ := range Articles {
+		if article.Id == id {
+			Articles[i] = article
+		}
+	}
 
 	json.NewEncoder(w).Encode(article)
 }
